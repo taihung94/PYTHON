d349f9fef284f0a516d20afc08e4a5ce5ff48e68
taihung94 <hungnthe171217@gmail.com>
2024-04-19 22:00:40 +0700

    Day1 learn Python

---
+++ Day1/hello.py
@@ -0,0 +1,5 @@
+#sử dụng format để định dạng căn lề
+print('{:^10}'.format('hung')) # căn giữa
+print('{:>10}'.format('hung')) #căn phải
+print('{:<10}'.format('hung')) #căn trái
+print('{:@>10}'.format('hung'))# căn phải thêm @
